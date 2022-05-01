29199a579d7d96378cefba77a314a4bdda0cd616
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-01 10:07:30 -0300

    Update MainActivity.java

--- ParOuImpar/app/src/main/java/com/henrique/ParOuImpar/MainActivity.java
+++ ParOuImpar/app/src/main/java/com/henrique/ParOuImpar/MainActivity.java
@@ -3,8 +3,12 @@ package com.henrique.ParOuImpar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtNum;
+    Button btnVerificar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
