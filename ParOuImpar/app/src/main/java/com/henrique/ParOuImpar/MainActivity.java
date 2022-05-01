29249b4adeca81a80b67da20efad3e158f05d4f1
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-01 10:35:50 -0300

    Update MainActivity.java

--- ParOuImpar/app/src/main/java/com/henrique/ParOuImpar/MainActivity.java
+++ ParOuImpar/app/src/main/java/com/henrique/ParOuImpar/MainActivity.java
@@ -1,10 +1,11 @@
 package com.henrique.ParOuImpar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtNum;
@@ -14,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNum = findViewById(R.id.edtNum);
+        btnVerificar = findViewById(R.id.btnVerificar);
+
+        String num = (edtNum.getText().toString());
+
+        btnVerificar.setOnClickListener(View -> {
+            if (num.length() == 0) {
+                Toast.makeText(this, "Caixa de texto vazia!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Preencha corretamente!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
